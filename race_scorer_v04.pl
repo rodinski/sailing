79a652abf0659d670f41bc0cb5ffdc12ae2b547f
rodinski <crimson.blue.2@gmail.com>
2018-11-22 15:38:48 -0600

    test with added comment

--- race_scorer_v04.pl
+++ race_scorer_v04.pl
@@ -19,6 +19,7 @@ use Data::Dumper;
 my $version_string = "race_scorer_v04.pl version 0.4 coded: 2016_07_25";
 
 my ($bt,$defaults) = LoadFile('./boats_2018.yaml');
+#  hardcode file
 my $r =  LoadFile('./2018_overall.yaml');
 my $table; #ref to a data structure for later results
 my $races_held = 0;  #counter
